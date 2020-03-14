d45b3ab265d67dee33ef27e811333e1eabffc2ac
kidevil <kidevil@outlook.com>
2020-03-14 22:34:13 +0800

    自己修改代码

--- git/src/main/java/com/itcast/gittest/GitTest.java
+++ git/src/main/java/com/itcast/gittest/GitTest.java
@@ -3,7 +3,7 @@ package com.itcast.gittest;
 /**
  * @Auther: 343
  * @Date: 2020/3/14 21:18
- * @Description: 这是一个git测试
+ * @Description: 这是一个git测试 自己的
  */
 public class GitTest {
     public static void main(String[] args) {
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("修改后的版本");
         System.out.println("老板修改的代码");
         System.out.println("自己修改的代码");
+        System.out.println("自己又加了行代码");
     }
 }
