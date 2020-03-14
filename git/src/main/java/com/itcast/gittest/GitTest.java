a965600876b73319970b3620d20e2b7bcbdde6c0
kidevil <kidevil@outlook.com>
2020-03-14 21:20:38 +0800

    Initial commit

---
+++ git/src/main/java/com/itcast/gittest/GitTest.java
@@ -0,0 +1,12 @@
+package com.itcast.gittest;
+
+/**
+ * @Auther: 343
+ * @Date: 2020/3/14 21:18
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git测试");
+    }
+}
