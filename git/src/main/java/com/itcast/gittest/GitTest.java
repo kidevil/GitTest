0ad7c5f1a895a24da06830feba972cb7483e4fb8
kidevil <kidevil@outlook.com>
2020-03-14 21:25:02 +0800

    新添加了数据

--- git/src/main/java/com/itcast/gittest/GitTest.java
+++ git/src/main/java/com/itcast/gittest/GitTest.java
@@ -3,10 +3,11 @@ package com.itcast.gittest;
 /**
  * @Auther: 343
  * @Date: 2020/3/14 21:18
- * @Description:
+ * @Description: 这是一个git测试
  */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git测试");
+        System.out.println("修改后的版本");
     }
 }
